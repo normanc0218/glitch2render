8f8835e90ab4e6b4599bc7a50c851da1d1a562ff
Glitch (ambiguous-ionized-traffic) <none>
2025-05-23 18:05:39 +0000

    📼💫 Checkpoint ./server.js:79339030/1302

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// 解析 JSON body
-app.use(bodyParser.json());
-
-// Slack Events API Endpoint
-app.post('/slack/events', async (req, res) => {
-  const { type } = req.body;
-
-  switch (type) {
-    case 'url_verification': {
-      // Step 1: 验证 Slack Events API 的 URL
-      return res.send({ challenge: req.body.challenge });
-    }
-
-    // 你可以在这里继续扩展其他事件类型，如 'event_callback'
-    
-    default:
-      return res.sendStatus(200);
-  }
+const axios =require('axios');
+const { App } = require('@slack/bolt');
+const signingSecret=process.env['SLACK_SIGNING_SECRET']
+const botToken = process.env['SLACK_BOT_TOKEN']
+const app = new App({
+  signingSecret: signingSecret,
+  token: botToken,
 });
 
-// 启动服务器
-app.listen(PORT, () => {
-  console.log(`⚡️ Server is running on port ${PORT}`);
-});
+(async () =>{
+  await app.start(prcoess.env.PORT || 12000);
+  
+  app.message('quote',async({message.say})=>{
+    let resp =await axios.get('https://api.quotable.io/random');
+    console.log(message);
+    const quote=resp.data.content;
+    await say('H')
+  })
+})
