ce6ea59ca163ef6b77dc3c83e01f27cb491aabbb
Glitch (ambiguous-ionized-traffic) <none>
2025-05-27 16:03:15 +0000

    🚔⛄️ Checkpoint ./openModal.js:79339030/3114

--- openModal.js
+++ openModal.js
@@ -2,65 +2,108 @@ const axios = require('axios');
 const qs = require('qs');
 const openModal = async(trigger_id) => {
   const modal = {
-    type: 'modal',
-    title: {
-      type: 'plain_text',
-      text: 'Maintenance Order Form'
-    },
-    submit: {
-      type: 'plain_text',
-      text: 'submit'
-    },
-    blocks: [
-      // Text input
-      {
-        "type": "input",
-        "block_id": "note01",
-        "label": {
-          "type": "plain_text",
-          "text": "Note"
-        },
-        "element": {
-          "action_id": "content",
-          "type": "plain_text_input",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Take a note... "
-          },
-          "multiline": true
-        }
-      },
-      // Drop-down menu
-      {
-        "type": "input",
-        "block_id": "note02",
-        "label": {
-          "type": "plain_text",
-          "text": "Color",
-        },
-        "element": {
-          "type": "static_select",
-          "action_id": "color",
-          "options": [
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "yellow"
-              },
-              "value": "yellow"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "blue"
-              },
-              "value": "blue"
-            }
-          ]
-        }
-      }
-    ]
-  };
+	"type": "modal",
+	"title": {
+		"type": "plain_text",
+		"text": "My App",
+		"emoji": true
+	},
+	"submit": {
+		"type": "plain_text",
+		"text": "Submit",
+		"emoji": true
+	},
+	"close": {
+		"type": "plain_text",
+		"text": "Cancel",
+		"emoji": true
+	},
+	"blocks": [
+		{
+			"type": "input",
+			"block_id": "note01",
+			"label": {
+				"type": "plain_text",
+				"text": "Machine and Location"
+			},
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "machine_location_input",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Where is the machine?"
+				}
+			}
+		},
+		{
+			"type": "input",
+			"block_id": "note02",
+			"label": {
+				"type": "plain_text",
+				"text": "Description of the Issue"
+			},
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "issue_description_input",
+				"multiline": true,
+				"placeholder": {
+					"type": "plain_text",
+					"text": "What is happening?"
+				}
+			}
+		},
+		{
+			"type": "input",
+			"block_id": "note03",
+			"label": {
+				"type": "plain_text",
+				"text": "Assign the job to"
+			},
+			"element": {
+				"type": "multi_static_select",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select the person",
+					"emoji": true
+				},
+				"options": [
+					{
+						"text": {
+							"type": "plain_text",
+							"text": "*Fai",
+							"emoji": true
+						},
+						"value": "value-0"
+					},
+					{
+						"text": {
+							"type": "plain_text",
+							"text": "Steven",
+							"emoji": true
+						},
+						"value": "value-1"
+					},
+					{
+						"text": {
+							"type": "plain_text",
+							"text": "Sam",
+							"emoji": true
+						},
+						"value": "value-2"
+					}
+				],
+				"action_id": "multi_static_select-action"
+			}
+		},
+		{
+			"type": "image",
+			"slack_file": {
+				"url": "<insert slack file url here>"
+			},
+			"alt_text": "inspiration"
+		}
+	]
+};
 
   const args = {
     token: process.env.SLACK_BOT_TOKEN,
