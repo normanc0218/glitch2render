c28c696dfc2293328d052c3074db7f703fea965d
Glitch (ambiguous-ionized-traffic) <none>
2025-05-30 01:47:36 +0000

    👏🎈 Checkpoint ./openModal_view_detail.js:79339030/501

--- openModal_view_detail.js
+++ openModal_view_detail.js
@@ -1,6 +1,21 @@
 const axios = require('axios');
 const qs = require('qs');
-const openModal = async(trigger_id) => {
+const nyDate = new Intl.DateTimeFormat('en-US', {
+  timeZone: 'America/New_York',
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit'
+}).format(new Date()); // e.g. "2025-05-28"
+const [month, day, year] = nyDate.split('/');
+const initialDate = `${year}-${month}-${day}`;
+
+const initialTime =  new Intl.DateTimeFormat("en-US", {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
+  timeZone: "America/New_York"
+}).format(new Date()); // e.g. "14:37"
+const openModal_view_detail = async(trigger_id) => {
   const modal = require('./modal_j.json');
 
   const args = {
@@ -11,4 +26,4 @@ const openModal = async(trigger_id) => {
   
   const result = await axios.post('https://slack.com/api/views.open', qs.stringify(args));
 };
-module.exports = { openModal};
+module.exports = { openModal_view_detail};
