22656515e2fdd483a2f1401154992e91e1a55df0
Glitch (ambiguous-ionized-traffic) <none>
2025-06-04 21:02:34 +0000

    👕😅 Checkpoint ./openModal_daily_job.js:79339030/4754 ./test.js:79339030/91

--- test.js
+++ test.js
@@ -3,4 +3,5 @@ const { fetchCalendar } = require('./fetchCalendar');
 (async () => {
   const events = await fetchCalendar('3c900c9ad4cfa608582d351a1cffae1c54c08ad48cab7be68eb3921305a88352@group.calendar.google.com');
   console.log(events); // Now you can safely use the result
+  
 })();
