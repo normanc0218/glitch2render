52a6cec08a4c19b358010a5c04aac824618c9131
Glitch (ambiguous-ionized-traffic) <none>
2025-06-06 16:50:24 +0000

    🦃🐿 Checkpoint ./openModal_supervisor_approval.js:79339030/5056

--- openModal_supervisor_approval.js
+++ openModal_supervisor_approval.js
@@ -1,4 +1,14 @@
 const axios = require('axios');
+const {
+  createInputBlock,
+  createInputBlock_select,
+  createTextSection,
+  createInputBlock_date,
+  createInputBlock_time,
+  createInputBlock_checkboxes,
+  createInputBlock_radio,
+  createInputBlock_pic
+} = require('./blockBuilder');
 const nyDate = new Intl.DateTimeFormat('en-US', {
   timeZone: 'America/New_York',
   year: 'numeric',
@@ -16,170 +26,46 @@ const initialTime =  new Intl.DateTimeFormat("en-US", {
 }).format(new Date()); // e.g. "14:37"
 
 // list of managerUser IDs
-const supervisors = {
-  // "Chris" : "U06D0NAAL5N",// Chris
-  "Norman":  "U06DSKC32E4",// Norman
-  "Justin": "U06D0NA0H16", // Justin
-  "Tim": "U06CBUTM4JW",// Tim
-  "Grace":"U0"
-};
+const {Supervisors} = require('./userConfig');
+const superOption=Object.entries(Supervisors)
 
 
 const openModal_supervisor_approval = async (trigger_id, jobId) => {
+  const blocks=[]
+  blocks.push(createInputBlock_select({
+    block_id: "tool_id",
+    label: "Assigned Maintenance has/have collected their tools and materials",
+    action_id: "Maitenance_tool",
+    options: ["Yes","No"], // <-- make sure this is passed in like this
+  }));
+  blocks.push(createInputBlock_select({
+    block_id: "clean_id",
+    label: "Working area needs extra helps for cleaning?",
+    action_id: "working_area",
+    options: ["Yes","No"], // <-- make sure this is passed in like this
+  }));
+  blocks.push(createInputBlock("clean_input", "Assign who to help cleaning?", "name_clean", "e.g. Somebody",true));
+  blocks.push(createInputBlock("other_input", "*Specify other details related to this job", "detailOfJob", "e.g. Something",true));
+
+  blocks.push(createInputBlock_date("date", "Check Date", "datepickeraction", initialDate));
+  blocks.push(createInputBlock_time("time", "Check Time", "timepickeraction", initialTime));
   const modal ={
-	"type": "modal",
-	"callback_id": "review_progress",
-	"private_metadata": jobId,
-	"title": {
-		"type": "plain_text",
-		"text": "Review progress"
+	type: "modal",
+	callback_id: "review_progress",
+	private_metadata: jobId,
+	title: {
+		type: "plain_text",
+		text: "Review progress"
 	},
-	"submit": {
-		"type": "plain_text",
-		"text": "Approved"
+	submit: {
+		type: "plain_text",
+		text: "Approved"
 	},
-	"close": {
-		"type": "plain_text",
-		"text": "Cancel"
+	close: {
+		type: "plain_text",
+		text: "Cancel"
 	},
-	"blocks": [
-		{
-			"type": "section",
-      "block_id":"tool_id",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Assigned Maintenance has/have collected their tools and materials*"
-			},
-			"accessory": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select an item",
-					"emoji": true
-				},
-				"options": [
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "Yes",
-							"emoji": true
-						},
-						"value": "Yes"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "No",
-							"emoji": true
-						},
-						"value": "No"
-					}
-				],
-				"action_id": "Maitenance_tool"
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Working area needs extra helps for cleaning?*"
-			},
-			"accessory": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select an item",
-					"emoji": true
-				},
-				"options": [
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "Yes",
-							"emoji": true
-						},
-						"value": "Yes"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "No",
-							"emoji": true
-						},
-						"value": "No"
-					}
-				],
-				"action_id": "working_are"
-			}
-		},
-		{
-			"type": "input",
-			"block_id": "clean_input",
-			"element": {
-				"type": "plain_text_input",
-				"action_id": "name_clean"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Assign who to help cleaning the working area?",
-				"emoji": true
-			},
-			"optional": true
-		},
-		{
-			"type": "input",
-			"block_id": "other_reason_input",
-			"element": {
-				"type": "plain_text_input",
-				"multiline": true,
-				"action_id": "detailOfJob"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "*Specify other details related to this job",
-				"emoji": true
-			},
-			"optional": true
-		},
-		{
-			"type": "input",
-			"block_id": "date",
-			"element": {
-				"type": "datepicker",
-				"initial_date": initialDate,
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select a date",
-					"emoji": true
-				},
-				"action_id": "datepickeraction"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Check date",
-				"emoji": true
-			}
-		},
-		{
-			"type": "input",
-			"block_id": "time",
-			"element": {
-				"type": "timepicker",
-				"initial_time": initialTime,
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select time",
-					"emoji": true
-				},
-				"action_id": "timepickeraction"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Check time",
-				"emoji": true
-			}
-		}
-	]
+	blocks
 }
 ;
 
