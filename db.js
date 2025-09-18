f5730ef40f4965116033614e2e171f58c3965ae0
normanc0218 <149454253+normanc0218@users.noreply.github.com>
2025-09-18 14:52:11 -0400

    Update db.js

--- db.js
+++ db.js
@@ -18,12 +18,7 @@
 // const db = admin.database();
 // module.exports = db;
 const admin = require("firebase-admin");
-console.log("DEBUG ENV KEYS:", Object.keys(process.env));
-console.log("DEBUG FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
-console.log("DEBUG FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
-console.log("DEBUG FIREBASE_DATABASE_URL:", process.env.FIREBASE_DATABASE_URL);
-console.log("DEBUG FIREBASE_PRIVATE_KEY type:", typeof process.env.FIREBASE_PRIVATE_KEY);
-console.log("DEBUG FIREBASE_PRIVATE_KEY length:", process.env.FIREBASE_PRIVATE_KEY?.length);
+
 // Railway 上直接用环境变量初始化，不需要 JSON 文件
 if (!admin.apps.length) {
   admin.initializeApp({
