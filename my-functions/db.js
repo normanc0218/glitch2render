3726e096a6eb54a67011d0633db6140525745bc6
normanc0218 <149454253+normanc0218@users.noreply.github.com>
2026-04-29 18:06:24 +0000

    fix db url

--- my-functions/db.js
+++ my-functions/db.js
@@ -1,23 +1,11 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
-// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: process.env.FIREBASE_URL
-// });
-
-// const db = admin.database();
-// module.exports = db;
-// 如果还没初始化，则初始化（本地和部署环境都兼容）
 if (!admin.apps.length) {
   admin.initializeApp({
-    // 在 Cloud Functions 部署环境中，不需要提供 credential 和 databaseURL
-    // 它们会自动从环境中获取
-    databaseURL: process.env.DATABASE_URL || "https://maintenance-form-602d9.firebaseio.com"
+    databaseURL: process.env.DATABASE_URL || "https://maintenance-form-602d9-default-rtdb.firebaseio.com"
   });
 }
 
 const db = admin.database();
-
 module.exports = db;
